7856394ca54e66e6056053c5fd99c1797519a6f1
Stephan Kostin <va.lenta0907@gmail.com>
2023-10-28 17:29:24 +0300

    Back to 1.0 version...

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,10 +1,10 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'; // take readline-sync to see your answ
 
 const getNumb = () => {
     return Math.round(Math.random() * 100); 
-};
+}
 
-const getSing = () => {
+function getSing() { // get sing for game
     const chance = getNumb();
     if (chance < 40) {
         return '+';
@@ -13,40 +13,42 @@ const getSing = () => {
     } else {
         return '*';
     }
-};
+}
 
-const calculateGame = () => {
-    console.log('What is the result of the expression?');
+function calculateGame() { // game || function
+    console.log('What is the result of the expression?'); // welcome logo
     for (let i = 0; i < 3; i += 1) {
         const numb = getNumb();
         const numb1 = getNumb();
-        const sing = getSing();
-        console.log(`Question: ${numb} ${sing} ${numb1}`);
-
-        const userAnsw = readlineSync.question('Your answer: ');
+        const sing = getSing(); // get all things 
+        console.log(`Question: ${numb} ${sing} ${numb1}`); // qustion
+        
+        const userAnsw= readlineSync.question('Your answer: ');
         let rightAnsw = 0;
-
+        
         if (sing === '*') {
             rightAnsw = numb * numb1;
         } else if (sing === '+'){
             rightAnsw = numb + numb1;
         } else { 
             rightAnsw = numb - numb1;
-        }
-
-        if (Number(userAnsw) === rightAnsw) {
+        } // find correct answer
+        
+        if (Number(userAnsw) === rightAnsw) { // check answ
             console.log('Correct!');
         } else {
             console.log(`'${userAnsw}' is wrong answer ;(. Correct answer was '${rightAnsw}'.`);
-            return;
+            return; // return || lose
         }
     }
-    console.log(`Congratulations, ${userName}!`);
-};
+    console.log(`Congratulations, ${userName}!`); // win
+}
 
 console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
 
-calculateGame();
+calculateGame(); // start game
+
+
