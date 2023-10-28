d81a970a12e173035cf5f1667fc76cd699aaa2c1
Stephan Kostin <va.lenta0907@gmail.com>
2023-10-28 17:33:56 +0300

    Update 1.1 (again)

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,53 +1,56 @@
 import readlineSync from 'readline-sync'; // take readline-sync to see your answ
 
-const getNumb = () => { // random number (1-100)
-    return Math.round(Math.random() * 100); 
-}
+function progressGame() {
+    console.log('What number is missing in the progression?');
+    for (let i = 0; i < 3; i += 1) {
+        const result = generateProgression();
+        const rightAnswer = askQuestion(result);
+        const userAnswer = getUserAnswer();
 
-const getMinNumb = () => { // random number (1-10)
-    return Math.round(Math.random() * 10);
-}
-
-function progressGame() { // function game
-    console.log('What number is missing in the progression?'); // weclome alert
-    for (let i = 0; i < 3; i += 1) { // try win
-        let result = [getNumb()];
-        let length = getMinNumb() + 3;
-        const getAdd = getNumb(); // take what we need
-    
-        if (length < 6) {
-            length = length + 5;
-        } // check not be too small
-        for (let i = 0; i < length; i += 1) {
-            result.push(result[i] + getAdd);
-        } // make array
-
-        const hole = length - getMinNumb(); // take random numb
-        result = result.sort((a, b) => a - b); // sort array
-        const rightAnsw = result[hole]; // find answer
-
-        result[hole] = '..'; // change answer
-        result = result.join(' '); // array => str
-
-        console.log(`Question: ${result}`); // ask qustion
-        
-        const userAnsw= readlineSync.question('Your answer: '); // take answ
-        
-        if (Number(userAnsw) === rightAnsw) { // check answ
+        if (userAnswer === rightAnswer) {
             console.log('Correct!');
         } else {
-            console.log(`'${userAnsw}' is wrong answer ;(. Correct answer was '${rightAnsw}'.`);
+            console.log(`'${userAnswer}' is the wrong answer ;(. Correct answer was '${rightAnswer}'.`);
             return;
         }
     }
-    console.log(`Congratulations, ${userName}!`); // win logo
+    console.log(`Congratulations, ${userName}!`);
+}
+
+function generateProgression() {
+    const result = [];
+    const getAdd = getNumb();
+    let length = getMinNumb() + 3;
+
+    if (length < 6) {
+        length += 5;
+    }
+
+    for (let i = 0; i < length; i += 1) {
+        result.push(result[i] + getAdd);
+    }
+
+    const hole = length - getMinNumb();
+    result[hole] = '..';
+    return result;
+}
+
+function askQuestion(result) {
+    result = result.sort((a, b) => a - b);
+    const rightAnswer = result.find((item) => item === '..');
+    result = result.join(' ');
+    console.log(`Question: ${result}`);
+    return rightAnswer;
+}
+
+function getUserAnswer() {
+    return readlineSync.question('Your answer: ');
 }
 
 console.log('Welcome to the Brain Games!');
-
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
+progressGame();
 
-progressGame(); // start
 
 
