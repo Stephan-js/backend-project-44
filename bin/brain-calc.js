d44a2062c8e95d93c8c8162eff6ee7c735f8f366
Stepan Kostin <va.lenta0907@gmail.com>
2023-11-02 15:31:23 +0300

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -3,49 +3,49 @@
 import readlineSync from 'readline-sync'; // take readline-sync to see your answ
 
 function getNumb() {
-    return Math.round(Math.random() * 100); 
-};
+  return Math.round(Math.random() * 100);
+}
 
 function getSing() { // get sing for game
-    const chance = getNumb();
-    if (chance < 40) {
-        return '+';
-    } 
-    if (chance < 80) {
-        return '-';
-    } 
-    
-    return '*';
+  const chance = getNumb();
+  if (chance < 40) {
+    return '+';
+  } 
+  if (chance < 80) {
+    return '-';
+  } 
+
+  return '*';
 }
 
 function calculateGame() { // game || function
-    console.log('What is the result of the expression?'); // welcome logo
-    for (let i = 0; i < 3; i += 1) {
-        const numb = getNumb();
-        const numb1 = getNumb();
-        const sing = getSing(); // get all things 
-        console.log(`Question: ${numb} ${sing} ${numb1}`); // qustion
-        
-        const userAnsw = readlineSync.question('Your answer: ');
-        let rightAnsw = 0;
-        
-        if (sing === '*') {
-            rightAnsw = numb * numb1;
-        } else if (sing === '+'){
-            rightAnsw = numb + numb1;
-        } else { 
-            rightAnsw = numb - numb1;
-        } // find correct answer
-        
-        if (Number(userAnsw) === rightAnsw) { // check answ
-            console.log('Correct!');
-        } else {
-            console.log(`'${userAnsw}' is wrong answer ;(. Correct answer was '${rightAnsw}'.`);
-            console.log(`Let's try again, ${userName}!`);
-            return; // return || lose
-        }
+  console.log('What is the result of the expression?'); // welcome logo
+  for (let i = 0; i < 3; i += 1) {
+    const numb = getNumb();
+    const numb1 = getNumb();
+    const sing = getSing(); // get all things 
+    console.log(`Question: ${numb} ${sing} ${numb1}`); // qustion
+
+    const userAnsw = readlineSync.question('Your answer: ');
+    let rightAnsw = 0;
+
+    if (sing === '*') {
+      rightAnsw = numb * numb1;
+    } else if (sing === '+'){
+      rightAnsw = numb + numb1;
+    } else { 
+      rightAnsw = numb - numb1;
+    } // find correct answer
+    
+    if (Number(userAnsw) === rightAnsw) { // check answ
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnsw}' is wrong answer ;(. Correct answer was '${rightAnsw}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return; // return || lose
     }
-    console.log(`Congratulations, ${userName}!`); // win
+  }
+  console.log(`Congratulations, ${userName}!`); // win
 }
 
 console.log('Welcome to the Brain Games!');
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -13,10 +13,11 @@ const getNumb = () => { // take numb or -numb
 
 const isPrime = (num) => {
     for (let i = 2, s = Math.sqrt(num); i <= s; i+= 1) {
-        if (num % i === 0) return false;
+      if (num % i === 0) return false;
     }
     return num > 1;
 };
+
 
 function primeGame() { // game logic
     console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
